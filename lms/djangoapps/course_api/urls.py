6e50601422754c0a9834fd265cc1c48e61f48df5
Abdul Rafay <arafay.knysys@gmail.com>
2020-12-09 12:37:16 +0000

    Completed Part1 for task: LMA59 > LMA 183 Backend_ Create new API for main page. Part1 include: API to get recommended courses

--- lms/djangoapps/course_api/urls.py
+++ lms/djangoapps/course_api/urls.py
@@ -7,9 +7,11 @@ from django.conf import settings
 from django.conf.urls import include, url
 
 from .views import CourseDetailView, CourseIdListView, CourseListView
+from .mobile_views import CourseListView as MobileCourseListView
 
 urlpatterns = [
     url(r'^v1/courses/$', CourseListView.as_view(), name="course-list"),
+    url(r'^v2/recommended/courses/$', MobileCourseListView.as_view(), name="course-list"),
     url(r'^v1/courses/{}'.format(settings.COURSE_KEY_PATTERN), CourseDetailView.as_view(), name="course-detail"),
     url(r'^v1/course_ids/$', CourseIdListView.as_view(), name="course-id-list"),
     url(r'', include('course_api.blocks.urls'))

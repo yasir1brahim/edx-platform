0c7562293f7dcffcb68b487de4ac3475eb5cbf0d
Zach Hancock <zhancock@edx.org>
2020-12-14 12:08:35 -0500

    eval query

--- openedx/core/djangoapps/enrollments/services.py
+++ openedx/core/djangoapps/enrollments/services.py
@@ -2,4 +2,4 @@ from common.djangoapps.student.models import CourseEnrollment, User
 
 class EnrollmentsService(object):
     def get_enrollments_by_course(self, course_id):
-        return CourseEnrollment.objects.filter(course_id=course_id)
+        return list(CourseEnrollment.objects.filter(course_id=course_id))

fdaaac9972f1c12973f572487bdf3527a936160d
Abdul Rafay <arafay.knysys@gmail.com>
2020-12-17 11:59:51 +0000

    LMA59>LMA-256: pagination of course list

--- lms/djangoapps/commerce/api/v2/views.py
+++ lms/djangoapps/commerce/api/v2/views.py
@@ -3,16 +3,19 @@ from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthenticat
 from openedx.core.lib.api.authentication import BearerAuthentication
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .filters import LazyPageNumberPagination
 
 from ..v1.models import Course
 from ..v1.serializers import CourseSerializer
 
 class CourseListView(ListAPIView):
     """ List courses and modes. """
+    class CourseListPageNumberPagination(LazyPageNumberPagination):
+        max_page_size = 100
     #authentication_classes = (JwtAuthentication, BearerAuthentication, SessionAuthentication,)
     #permission_classes = (IsAuthenticated,)
     serializer_class = CourseSerializer
-    pagination_class = None
+    pagination_class = CourseListPageNumberPagination
 
     def get_queryset(self):
         return list(Course.iterator())

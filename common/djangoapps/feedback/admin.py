88d2df6d6958ee26941d63a6676b2d390369eca2
Abdul Rafay <arafay.knysys@gmail.com>
2021-01-01 11:43:02 +0000

    Showing created_at in admin panel for course review.

--- common/djangoapps/feedback/admin.py
+++ common/djangoapps/feedback/admin.py
@@ -4,5 +4,7 @@ from .models import CourseReview
 
 # Register your models here.
 
-
-admin.site.register(CourseReview)
+#admin.site.register(CourseReview)
+@admin.register(CourseReview)
+class CourseReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', )

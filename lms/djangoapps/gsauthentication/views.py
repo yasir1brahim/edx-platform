a7b5c89c2d2729c8aa717b3c7e6a352f6c2b2d3f
Abdul Rafay <arafay.knysys@gmail.com>
2021-01-07 06:02:24 +0000

    Use email and username both for Bearer token access in gsauthentication.

--- lms/djangoapps/gsauthentication/views.py
+++ lms/djangoapps/gsauthentication/views.py
@@ -6,15 +6,19 @@ from rest_framework.views import APIView
 from django.contrib.auth.models import User
 import requests
 import json 
-
+from django.conf import settings
 
 class CustomObtainAuthToken(APIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            user = User.objects.get(username=request.POST.get("username"))
+            user_exist = User.objects.filter(email=request.POST.get("username")).exists()
+            if user_exist:
+                user = User.objects.get(email=request.POST.get("username"))
+            else:
+                user = User.objects.get(username=request.POST.get("username"))
             if user.is_active:
-                response = requests.post('http://134.209.96.11/oauth2/access_token', params=request.POST)
+                response = requests.post(settings.LMS_ROOT_URL+"/oauth2/access_token", params=request.POST)
                 resp_json = response.json()
                 if "access_token" in resp_json:
                     resp_json['username'] = user.username

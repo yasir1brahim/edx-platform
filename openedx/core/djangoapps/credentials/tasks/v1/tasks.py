01821140767722e02b52a0bf34610183cd209ad1
oliviaruizknott <olivia@ruiz-knott.com>
2021-04-13 11:07:30 -0600

    perf: move notify_credentials to a celery task
    Before this change, the notify_credentials management command ran for
    about 10-15 minutes and occasionally overlapped with an edxapp
    deployment which would cause the machine the command was running
    on to die. The command ran through a bunch of certs and grades, and then
    spun off celery tasks to send the relevant data to credentials.
    
    Now, most of the logic of the notify_credentials management command has
    been moved into a new celery task, so that combing through longs lists
    of certs and grades is done asynchronously. This task then spins off the
    original data-sending tasks as before.
    
    MICROBA-963

--- openedx/core/djangoapps/credentials/tasks/v1/tasks.py
+++ openedx/core/djangoapps/credentials/tasks/v1/tasks.py
@@ -2,6 +2,8 @@
 This file contains celery tasks for credentials-related functionality.
 """
 
+import math
+import time
 
 from celery import shared_task
 from celery.exceptions import MaxRetriesExceededError
@@ -10,8 +12,14 @@ from django.conf import settings
 from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
 from edx_django_utils.monitoring import set_code_owner_attribute
 from opaque_keys.edx.keys import CourseKey
+from MySQLdb import OperationalError
 
 from openedx.core.djangoapps.credentials.utils import get_credentials_api_client
+from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
+from lms.djangoapps.certificates.api import get_recently_modified_certificates
+from lms.djangoapps.grades.api import get_recently_modified_grades
+from lms.djangoapps.certificates.models import CertificateStatuses
+from openedx.core.djangoapps.programs.signals import handle_course_cert_changed, handle_course_cert_awarded
 
 logger = get_task_logger(__name__)
 
@@ -54,3 +62,178 @@ def send_grade_to_credentials(self, username, course_run_key, verified, letter_g
             f"Failed to send grade to credentials. Reason: {error_msg}"
         )
         raise self.retry(exc=exception, countdown=countdown, max_retries=MAX_RETRIES)
+
+
+@shared_task(bind=True, ignore_result=True)
+@set_code_owner_attribute
+def handle_notify_credentials(self, options, course_keys):
+    """
+    Celery task to handle the notify_credentials management command. Finds the
+    relevant cert and grade records, then starts other celery tasks to send the
+    data.
+    """
+
+    try:
+        site_config = SiteConfiguration.objects.get(site__domain=options['site']) if options['site'] else None
+    except SiteConfiguration.DoesNotExist:
+        logger.error('No site configuration found for site %s', options['site'])
+
+    certs = get_recently_modified_certificates(
+        course_keys, options['start_date'], options['end_date'], options['user_ids']
+    )
+
+    users = None
+    if options['user_ids']:
+        users = User.objects.filter(id__in=options['user_ids'])
+
+    grades = get_recently_modified_grades(
+        course_keys, options['start_date'], options['end_date'], users
+    )
+
+    logger.info('notify_credentials Sending notifications for {certs} certificates and {grades} grades'.format(
+        certs=certs.count(),
+        grades=grades.count()
+    ))
+
+    if options['dry_run']:
+        print_dry_run(certs, grades)
+    else:
+        send_notifications(
+            certs,
+            grades,
+            site_config=site_config,
+            delay=options['delay'],
+            page_size=options['page_size'],
+            verbose=options['verbose'],
+            notify_programs=options['notify_programs']
+        )
+
+    logger.info('notify_credentials finished')
+
+
+def send_notifications(
+    certs, grades, site_config=None, delay=0, page_size=100, verbose=False, notify_programs=False
+):
+    """ Run actual handler commands for the provided certs and grades. """
+
+    # Imported here instead of above to avoid circular import
+    from openedx.core.djangoapps.credentials.signals import send_grade_if_interesting
+
+    course_cert_info = {}
+    # First, do certs
+    for i, cert in paged_query(certs, delay, page_size):
+        if site_config and not site_config.has_org(cert.course_id.org):
+            logger.info("Skipping credential changes %d for certificate %s", i, certstr(cert))
+            continue
+
+        logger.info(
+            "Handling credential changes %d for certificate %s",
+            i, certstr(cert),
+        )
+
+        signal_args = {
+            'sender': None,
+            'user': cert.user,
+            'course_key': cert.course_id,
+            'mode': cert.mode,
+            'status': cert.status,
+            'verbose': verbose,
+        }
+
+        data = {
+            'mode': cert.mode,
+            'status': cert.status
+        }
+
+        course_cert_info[(cert.user.id, str(cert.course_id))] = data
+        handle_course_cert_changed(**signal_args)
+        if notify_programs and CertificateStatuses.is_passing_status(cert.status):
+            handle_course_cert_awarded(**signal_args)
+
+    # Then do grades
+    for i, grade in paged_query(grades, delay, page_size):
+        if site_config and not site_config.has_org(grade.course_id.org):
+            logger.info("Skipping grade changes %d for grade %s", i, gradestr(grade))
+            continue
+
+        logger.info(
+            "Handling grade changes %d for grade %s",
+            i, gradestr(grade),
+        )
+
+        user = User.objects.get(id=grade.user_id)
+
+        # Grab mode/status from cert call
+        key = (user.id, str(grade.course_id))
+        cert_info = course_cert_info.get(key, {})
+        mode = cert_info.get('mode', None)
+        status = cert_info.get('status', None)
+
+        send_grade_if_interesting(
+            user,
+            grade.course_id,
+            mode,
+            status,
+            grade.letter_grade,
+            grade.percent_grade,
+            verbose=verbose
+        )
+
+
+def paged_query(queryset, delay, page_size):
+    """
+    A generator that iterates through a queryset but only resolves chunks of it at once, to avoid overwhelming memory
+    with a giant query. Also adds an optional delay between yields, to help with load.
+    """
+    count = queryset.count()
+    pages = int(math.ceil(count / page_size))
+
+    for page in range(pages):
+        page_start = page * page_size
+        page_end = page_start + page_size
+        subquery = queryset[page_start:page_end]
+
+        if delay and page:
+            time.sleep(delay)
+        index = 0
+        try:
+            for item in subquery.iterator():
+                index += 1
+                yield page_start + index, item
+        except OperationalError:
+            # When running the notify_credentials command locally there is an
+            # OperationalError thrown by MySQL when there are no more results
+            # available for the queryset iterator. This change catches that exception,
+            # checks state, and then logs according to that state. This code runs in
+            # production without issue. This changes allows for the code to be run
+            # locally without a separate code path.
+            if index == count:
+                logger.info('OperationalError Exception caught, all known results processed in paged_query')
+            else:
+                logger.warning('OperationalError Exception caught, it is possible some results were missed')
+            continue
+
+def print_dry_run(certs, grades):
+    """Give a preview of what certs/grades we will handle."""
+    print("DRY-RUN: This command would have handled changes for...")
+    ITEMS_TO_SHOW = 10
+
+    print(certs.count(), "Certificates:")
+    for cert in certs[:ITEMS_TO_SHOW]:
+        print("   ", certstr(cert))
+    if certs.count() > ITEMS_TO_SHOW:
+        print("    (+ {} more)".format(certs.count() - ITEMS_TO_SHOW))
+
+    print(grades.count(), "Grades:")
+    for grade in grades[:ITEMS_TO_SHOW]:
+        print("   ", gradestr(grade))
+    if grades.count() > ITEMS_TO_SHOW:
+        print("    (+ {} more)".format(grades.count() - ITEMS_TO_SHOW))
+
+
+def certstr(cert):
+    return f'{cert.course_id} for user {cert.user.username}'
+
+
+def gradestr(grade):
+    return f'{grade.course_id} for user {grade.user_id}'

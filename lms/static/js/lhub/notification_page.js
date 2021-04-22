3cc4a5d76b47ccd118a9ddc276e73d891358dd43
Vladyslav Zherebkin <vladyslav.zherebkin@raccoongang.com>
2021-04-22 17:07:01 +0300

    Implemented unread feature for notification

--- lms/static/js/lhub/notification_page.js
+++ lms/static/js/lhub/notification_page.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
   'use strict';
 
-  $('.js-mark-as-read').on('click', function(ev) {
+  $('.js-mark-read-status').on('click', function(ev) {
     ev.preventDefault();
     var $ev = $(ev.currentTarget);
     var url = $ev.data('url');
+    var isRead = $ev.data('is-read');
     $.ajax({
         url: url,
         type: 'PUT',
-        data: {is_read: true}
+        data: {is_read: !isRead},
+        success: function (result) {
+          if (!isRead) {
+            $ev.parents('.js-item-notification').removeClass('new-notification');
+            $ev.children('.js-mark-as-read').addClass('hidden');
+            $ev.children('.js-mark-as-unread').removeClass('hidden');
+          }
+          else {
+            $ev.parents('.js-item-notification').addClass('new-notification');
+            $ev.children('.js-mark-as-read').removeClass('hidden');
+            $ev.children('.js-mark-as-unread').addClass('hidden');
+          }
+          $ev.data('is-read', !isRead);
+        },
     });
-    $ev.parents('.new-notification').removeClass('new-notification');
-    $ev.remove();
   });
 
   $('.js-delete-notification').on('click', function(ev) {

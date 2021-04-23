6d874aca1217dc9834a50d4e86bd92051d67d661
thiruvalluvan <thiruvalluvan@ntuclearninghub.com>
2021-04-23 21:57:01 +0800

    LMA-752: url fix 	modified:   lms/djangoapps/banner/api/serializers.py

--- lms/djangoapps/banner/api/serializers.py
+++ lms/djangoapps/banner/api/serializers.py
@@ -4,7 +4,6 @@ Serializers for Banner
 from rest_framework import serializers
 from lms.djangoapps.banner.models import Banner
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from django.contrib.sites.shortcuts import get_current_site
 from logging import getLogger
 log = getLogger(__name__)
 
@@ -26,15 +25,10 @@ class BannerSerializer(serializers.ModelSerializer):
             return None
 
     def banner_img_url_(self, obj):
-        try:
-            domain = str(get_current_site(request=None))
-        except Exception as ex:
-            log.error("Error while trying to fetch sites from Banner API " + str(ex))
-        http = "http://"
-        if http and domain and obj.banner_img_url_txt:
-            return http+domain+str(obj.banner_img_url_txt)
-        else:
+        if obj:
             return str(obj.banner_img_url_txt)
+        else:
+            return None
 
     class Meta(object):
         model = Banner

3ce9484cbf230de90b7713f523e1a5843258ec56
Vladyslav Zherebkin <vladyslav.zherebkin@raccoongang.com>
2021-05-07 17:29:39 +0300

    Implemented detail order API

--- lms/djangoapps/lhub_extended_api/urls.py
+++ lms/djangoapps/lhub_extended_api/urls.py
@@ -3,4 +3,5 @@ from lms.djangoapps.lhub_extended_api import views
 
 urlpatterns = [
     url(r'^orders$', views.LHUBOrdersHistoryView.as_view()),
+    url(r'^orders/(?P<order_number>[-\w]+)$', views.LHUBOrderDetailView.as_view()),
 ]

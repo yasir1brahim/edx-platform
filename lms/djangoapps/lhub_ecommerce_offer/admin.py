9c83b686c854f80907c8a285ecf1e3281bf21a72
Abdul Rafay <arafay.knysys@gmail.com>
2021-05-10 14:27:21 +0500

    LMA-535: Registered models in admin.py lhub_ecommerce_offer

--- lms/djangoapps/lhub_ecommerce_offer/admin.py
+++ lms/djangoapps/lhub_ecommerce_offer/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Offer, Course
 
 # Register your models here.
+
+class OfferAdmin(admin.ModelAdmin):
+    """ Admin class for Offer model """
+    fields = (
+        'incentive_type',
+        'incentive_value',
+        'condition_type',
+        'condition_value',
+        'start_datetime',
+        'end_datetime',
+        'priority',
+        'is_exclusive',
+        'associated_ecommerce_offer_id'
+    )
+    list_display = [
+        'incentive_type',
+        'incentive_value',
+        'condition_type',
+        'condition_value',
+        'start_datetime',
+        'end_datetime',
+        'priority',
+        'is_exclusive',
+        'associated_ecommerce_offer_id'
+    ]
+
+class CourseAdmin(admin.ModelAdmin):
+    """ Admin class for Course model """
+    fields = ('course',)
+    list_display = ['course']
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Course, CourseAdmin)
--- lms/djangoapps/lhub_ecommerce_offer/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.conf.urls import include, url
-from django.conf import settings
-from .views import UserSessionCookieView, CourseRetrieveUpdateView
-
-
-
